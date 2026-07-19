0390ea028ff6e900e0084459e2d710b2442a96a6
robinsalaria01 <robinra0045p@gmail.com>
2026-07-19 21:58:07 +0530

    Helps To find Biggest number in array

---
+++ arrBigNum.java
@@ -0,0 +1,17 @@
+package array;
+import java.util.Scanner;
+public class arrBigNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int arr[]= {2,3,1,8,4};
+        int n = arr.length;
+        int cmp=arr[0];
+        for(int i=0; i<n; i++){
+                if (cmp<=arr[i]){
+                    cmp=arr[i];
+                }
+        }
+        System.out.println(cmp);
+    }
+    
+}
